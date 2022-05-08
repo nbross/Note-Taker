de3a04a2adcaa5c32078044234fe13e3fa228c94
nbross <nbross2001@gmail.com>
2022-05-08 15:58:35 -0400

    delete request

--- server.js
+++ server.js
@@ -34,6 +34,25 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './develop/public/index.html'));
 });
 
+app.delete('/api/notes/:id', function (req, res) {
+    console.log("Req.params:", req.params);
+    const deleteNote = parseInt(req.params.id);
+    console.log(deleteNote);
+
+    for (let i = 0; i < dbJson.length; i++) {
+        if (deleteNote === dbJson[i].id) {
+            dbJson.splice(i, 1);
+
+            let noteJson = JSON.stringify(dbJson, null, 2);
+            writeFileAsync("./develop/db/db.json", noteJson).then(function () {
+                console.log("Your note has been deleted!");
+            });
+        }
+    }
+    res.json(dbJson);
+});
+
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
