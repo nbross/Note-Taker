be115353743b23444f5b734185e22e6678f9abed
nbross <nbross2001@gmail.com>
2022-05-08 15:14:10 -0400

    added get and post requests for notes

--- server.js
+++ server.js
@@ -9,8 +9,30 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('./develop/public'));
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './develop/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './develop/db/db.json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    res.json(parseNotes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './develop/db/db.json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    parseNotes.push(req.body);
+
+    fs.writeFileSync(path.join(__dirname, './develop/db/db.json'), JSON.stringify(parseNotes), "utf-8");
+    res.json("You have successfully added a note!");
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './develop/public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
